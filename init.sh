d21b5acf57d3c971c416f1a6ae3877de4602ecd4
legends-killer <yyy@legends-killer.cq.cn>
2023-07-30 14:25:58 +0800

    chore: fix typo

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
  # @Author: legends-killer
  # @Date: 2023-07-30 14:19:14
  # @LastEditors: legends-killer
- # @LastEditTime: 2023-07-30 14:21:47
+ # @LastEditTime: 2023-07-30 14:24:33
  # @Description:
 ### 
-cp config_example.json config.json
+cp config.example.json config.json
 cp lastRename.example.json lastRename.json
